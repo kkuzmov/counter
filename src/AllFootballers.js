a794bd7886053b2d392bdb5fbfc85973f99b72c7
Krum Kuzmov <kkuzmov@gmail.com>
2021-06-12 18:18:58 +0300

    add switch statement

--- src/AllFootballers.js
+++ src/AllFootballers.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function AllFootballers({footballers}) {
-  console.log(footballers)
   return (
       <table className="table">
         <thead>
--- src/CardMaker.js
+++ src/CardMaker.js
@@ -14,17 +14,33 @@ function CardMaker({ addFootballer }) {
     setClub("");
   };
 
-  const handleFullNameChange = (e) => {
-    let fullName = e.target.value;
-    setfullName(fullName);
-  };
-  const handlePositionChange = (e) => {
-    let position = e.target.value;
-    setPosition(position);
-  };
-  const handleClubChange = (e) => {
-    let club = e.target.value;
-    setClub(club);
+  // const handleFullNameChange = (e) => {
+  //   let fullName = e.target.value;
+  //   setfullName(fullName);
+  // };
+  // const handlePositionChange = (e) => {
+  //   let position = e.target.value;
+  //   setPosition(position);
+  // };
+  // const handleClubChange = (e) => {
+  //   let club = e.target.value;
+  //   setClub(club);
+  // };
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    switch (e.target.name) {
+      case "fullName":
+        setfullName(value);
+        break;
+      case "position":
+        setPosition(value);
+        break;
+      case "club":
+        setClub(value);
+        break;
+      default:
+    }
   };
 
   return (
@@ -35,21 +51,21 @@ function CardMaker({ addFootballer }) {
           type="text"
           name="fullName"
           value={fullName}
-          onChange={handleFullNameChange}
+          onChange={handleChange}
         />
         <input
           className={styles.cardMakerInput}
           type="text"
           name="position"
           value={position}
-          onChange={handlePositionChange}
+          onChange={handleChange}
         />
         <input
           className={styles.cardMakerInput}
           type="text"
           name="club"
           value={club}
-          onChange={handleClubChange}
+          onChange={handleChange}
         />
         <input type="submit" value="Submit"></input>
       </form>
